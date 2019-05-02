e84823a20ca2c4875ebcf9af9f4a34da700a5753
jordan <jordanovsj@gmail.com>
2019-05-02 14:32:42 +0100

    POVMsssssssssss

--- POVM.py
+++ POVM.py
@@ -1,6 +1,5 @@
 from pyquil import Program, get_qc
-from pyquil.gates import CNOT, H, MEASURE, RESET, RX, RY, RZ
-from typing import List
+from pyquil.gates import CNOT, CCNOT, H, MEASURE, RESET, RX, RY, RZ, X, Z, SWAP, CSWAP
 import time
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,59 +7,204 @@ import sys
 
 
 def nCU1(axis, angle, ctrls, target) -> Program:
+    """
+    :param axis: {'x', 'y', 'z'}
+    :param angle: REAL
+    :param ctrls: list(int)
+    :param target: int
+    :return: Program
+    """
 
-    program = Program()
+    nCU1_program = Program()
     n_ctrls = len(ctrls)
 
     if n_ctrls == 1:
         if axis == 'x':
-            program.inst(RX(angle/2, target))
-            program.inst(CNOT(ctrls[0], target))
-            program.inst(RX(-angle/2, target))
-            program.inst(CNOT(ctrls[0], target))
+            nCU1_program.inst(RX(angle/2, target))
+            nCU1_program.inst(CNOT(ctrls[0], target))
+            nCU1_program.inst(RX(-angle/2, target))
+            nCU1_program.inst(CNOT(ctrls[0], target))
         elif axis == 'y':
-            program.inst(RY(angle/2, target))
-            program.inst(CNOT(ctrls[0], target))
-            program.inst(RY(-angle/2, target))
-            program.inst(CNOT(ctrls[0], target))
+            nCU1_program.inst(RY(angle/2, target))
+            nCU1_program.inst(CNOT(ctrls[0], target))
+            nCU1_program.inst(RY(-angle/2, target))
+            nCU1_program.inst(CNOT(ctrls[0], target))
         elif axis == 'z':
-            program.inst(RZ(angle/2, target))
-            program.inst(CNOT(ctrls[0], target))
-            program.inst(RZ(-angle/2, target))
-            program.inst(CNOT(ctrls[0], target))
+            nCU1_program.inst(RZ(angle/2, target))
+            nCU1_program.inst(CNOT(ctrls[0], target))
+            nCU1_program.inst(RZ(-angle/2, target))
+            nCU1_program.inst(CNOT(ctrls[0], target))
         else:
             raise Exception(ValueError('Invalid axis value!'))
     elif n_ctrls > 1:
-        program.inst(nCU1(axis, angle/2, ctrls[1:], target))
-        program.inst(CNOT(ctrls[0], target))
-        program.inst(nCU1(axis, -angle/2, ctrls[1:], target))
-        program.inst(CNOT(ctrls[0], target))
+        nCU1_program.inst(nCU1(axis, angle/2, ctrls[1:], target))
+        nCU1_program.inst(CNOT(ctrls[0], target))
+        nCU1_program.inst(nCU1(axis, -angle/2, ctrls[1:], target))
+        nCU1_program.inst(CNOT(ctrls[0], target))
     else:
         raise Exception(ValueError('Missing control qubits!'))
 
-    return program
+    return nCU1_program
+
+
+def POVM_module_1(theta1, theta2, phi1, phi2, ancilla, target)-> Program:
+    """
+    :param theta1: REAL
+    :param theta2: REAL
+    :param phi1: REAL
+    :param phi2: REAL
+    :param ancilla: list(int)
+    :param target: int
+    :return: Program
+    """
+
+    POVM_module_1_program = Program()
+
+    # BS1: split paths p1 and p2
+    POVM_module_1_program.inst(CNOT(target, ancilla[0]))
+
+    # Controlled rotation for path p1
+    if theta1 != 0:
+        POVM_module_1_program.inst(X(ancilla[0]))
+        POVM_module_1_program.inst(nCU1('y', 2*(theta1 + np.pi/2), [ancilla[0]], target))  # not sure if angle should not be 2x
+        POVM_module_1_program.inst(X(ancilla[0]))
+
+    # Controlled rotation for path p2
+    if theta2 != 0:
+        POVM_module_1_program.inst(nCU1('y', 2*theta2, [ancilla[0]], target))
+
+    # Instead of branching to t-channel, perform swap of the ancilla qubit and the target
+    POVM_module_1_program.inst(SWAP(target, ancilla[0]))
+
+    # for the sake of clarity
+    POVM_module_1_program.inst(X(ancilla[0]))
+    POVM_module_1_program.inst(Z(ancilla[1]))
+
+    # phase shifts
+    if phi1 != 0:
+        POVM_module_1_program.inst(X(ancilla[0]))
+        POVM_module_1_program.inst(nCU1('z', phi1, [ancilla[0]], target))
+        POVM_module_1_program.inst(X(ancilla[0]))
+
+    if phi2 != 0:
+        POVM_module_1_program.inst(nCU1('z', phi2, [ancilla[0]], target))
+
+    return POVM_module_1_program
+
+
+def POVM_module_2(theta3, theta4)-> Program:
+    """
+    :param theta3: REAL
+    :param theta4: REAL
+    :return: Program
+    """
+
+    POVM_module_2_program = Program()
+
+    # bs2: split p3 and p4 channels
+    POVM_module_2_program.inst(CCNOT(target, ancilla[0], ancilla[1]))
+
+    # p3 rotation
+    POVM_module_2_program.inst(X(ancilla[1]))
+    POVM_module_2_program.inst(nCU1('y', 2*(theta3 + np.pi/2), ancilla, target))
+    POVM_module_2_program.inst(X(ancilla[1]))
+
+    # p4 rotation
+    POVM_module_2_program.inst(nCU1('y', 2*theta4, ancilla, target))
+
+    # SWAP
+    POVM_module_2_program.inst(CSWAP(ancilla[0], target, ancilla[1]))
+    POVM_module_2_program.inst(CNOT(ancilla[0], ancilla[1]))  # ??
+    POVM_module_2_program.inst(nCU1('z', np.pi, [ancilla[0]], ancilla[1]))  # fix a minus sign (not really necessary)
+
+    return POVM_module_2_program
 
 
 if __name__ == '__main__':
 
-    lattice = "Aspen-4-3Q-A"
-    qubits = [17, 10, 11]  # check if still correct
+    # parameters
+    simulation = (int(sys.argv[1]) == 1)
+    POVM_parts = int(sys.argv[2])
+    Kraus = int(sys.argv[3])
 
-    qpu = get_qc(lattice, as_qvm=True)
+    print('Simulation=' + str(simulation))
+    print('POVM_parts='+str(POVM_parts))
+    print('KRAUS ops='+str(Kraus))
+
+    if POVM_parts == 2:
+        # initial state params
+        theta0 = 2*np.pi/3
+        phi0 = 0
+
+        # module 1 parameters
+        theta1 = np.pi/4
+        theta2 = np.pi/4
+        phi1 = 0
+        phi2 = 0
+
+    elif POVM_parts == 3:
+        theta0 = 0
+        phi0 = 0
+
+        theta1 = np.arccos(np.sqrt(2/3))
+        theta2 = np.pi/2
+        theta3 = 0
+        theta4 = np.pi/2
+        phi1 = 0
+        phi2 = 0
+
+        alpha_ui = 0
+        alpha_uii = -np.pi / 2
+
+    else:
+        raise Exception('Wrong arguments!')
+
+    lattice = "Aspen-4-3Q-A"
+
+    # for the purpose of POVM we choose the most connected qubit (10 in the case) to be the measured one
+    qubits = [11, 10, 17]  # check if still correct
+    ancilla = [11, 17]
+    target = 10
+
+    qpu = get_qc(lattice, as_qvm=simulation)
 
     print(f'All qubits on {lattice}: {qpu.device.qubits()}')
     print(f'\nSelected qubits: {qubits}')
 
     program = Program(RESET())
-    program.inst(H(qubits[0]))
-    program.inst(H(qubits[1]))
-    program.inst(nCU1('y', np.pi/3, qubits[:2], qubits[-1]))
+    # create initial state
+    if theta0 != 0:
+        program.inst(RY(theta0, target))
+    if phi0 != 0:
+        program.inst(RZ(phi0, target))
+
+    # 1st AP module
+    program.inst(POVM_module_1(theta1, theta2, phi1, phi2, ancilla, target))
+
+    # 2nd AP module (+ U2, T1, T2, T3)
+    if POVM_parts == 3:
+        program.inst(nCU1('y', alpha_uii, [ancilla[0]], target))
+        program.inst(POVM_module_2(theta3, theta4))
+
+        # Kraus operators (Tsss)
+        if Kraus == 1:
+            # T2
+            program.inst(X(ancilla[1]))
+            program.inst(nCU1('y', 2*np.pi/3, ancilla, target))
+            program.inst(X(ancilla[1]))
+
+            # T3
+            program.inst(nCU1('y', 7*np.pi/3, ancilla, target))
+
+        # fix paths encodings: 11->01
+        program.inst(CNOT(ancilla[1], ancilla[0]))
+
     # Measurements
     ro = program.declare('ro', 'BIT', len(qubits))
     program.inst([MEASURE(qubit, ro[idx]) for idx, qubit in enumerate(qubits)])
 
     # No shots
-    program.wrap_in_numshots_loop(1000)
+    program.wrap_in_numshots_loop(100000)
     # Compile to QPU
     binary_reset = qpu.compile(program)
 
@@ -92,5 +236,5 @@ if __name__ == '__main__':
     print(counts)
     print(output_vector)
 
-    # plt.hist(results)
-    # plt.show()
+    plt.hist(results)
+    plt.show()
